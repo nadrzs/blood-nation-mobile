b8404d4b57b42e036571626ccaf1bdffb7793d89
nadrzs <nadhifarachma@yahoo.com>
2024-03-25 15:50:03 +0700

    Add Bottom Navbar

--- blood_nation/lib/pages/home.dart
+++ blood_nation/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +9,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  // Screen
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text("Home", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600)),
+    Text("Reservation",
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600)),
+    Text("Profile", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600))
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,24 +26,48 @@ class _HomeState extends State<Home> {
         title: const Text("BloodNation"),
         centerTitle: true,
         actions: <Widget>[
-          IconButton(
-            onPressed: (){}, 
-            icon: const Icon(Icons.notifications)
-          )
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications))
         ],
         backgroundColor: const Color.fromARGB(255, 156, 200, 222),
-        leading: IconButton(
-          onPressed: (){}, 
-          icon: const Icon(Icons.menu)
-        ),
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
         shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(25),
-            bottomRight: Radius.circular(25)
-          )
-        ),
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25))),
       ),
-      
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [BoxShadow(blurRadius: 20, color: Colors.blue)]),
+        child: SafeArea(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+          child: GNav(
+            rippleColor: Colors.grey[300]!,
+            hoverColor: Colors.grey[100]!,
+            gap: 8,
+            activeColor: Colors.blue,
+            iconSize: 24,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            tabBackgroundColor: Colors.grey[100]!,
+            color: Colors.black,
+            tabs: [
+              GButton(icon: Icons.home, text: "Home"),
+              GButton(icon: Icons.menu, text: "Reservation"),
+              GButton(icon: Icons.person, text: "Person")
+            ],
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+        )),
+      ),
     );
   }
 }
