51d0a1433d84b34261a78a9a47427035583d6fa9
Nadd <nadhifarachma@yahoo.com>
2024-05-03 16:35:26 +0700

    Add Image in Profile Page

--- blood_nation/lib/pages/profile.dart
+++ blood_nation/lib/pages/profile.dart
@@ -17,53 +17,60 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          const SizedBox(height: 20),
-          Text(
-            "Profile",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 60),
-          ItemProfile(
-              title: "Username", subtitle: username, iconData: Icons.person),
-          SizedBox(height: 40),
-          ItemProfile(
-              title: "Email",
-              subtitle: email,
-              iconData: Icons.email),
-          SizedBox(height: 40),
-          ItemProfile(
-              title: "Phone Number",
-              subtitle: phoneNumber,
-              iconData: Icons.phone),
-          SizedBox(height: 60),
-          Container(
-            padding: EdgeInsets.only(top: 3, left: 3),
-            child: MaterialButton(
-              minWidth: double.infinity,
-              height: 60,
-              elevation: 0,
-              color: Color(0xffC31C2B),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => LoginPage()));
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)),
-              child: Text(
-                "Log Out",
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                    color: Colors.white),
+        body: SingleChildScrollView(
+          child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+          children: [
+            const SizedBox(height: 30),
+            Text(
+              "Profile",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 50),
+              height: 200,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/profile.png"),
+                      fit: BoxFit.fitHeight)),
+            ),
+            const SizedBox(height: 30),
+            ItemProfile(
+                title: "Username", subtitle: username, iconData: Icons.person),
+            SizedBox(height: 40),
+            ItemProfile(title: "Email", subtitle: email, iconData: Icons.email),
+            SizedBox(height: 40),
+            ItemProfile(
+                title: "Phone Number",
+                subtitle: phoneNumber,
+                iconData: Icons.phone),
+            SizedBox(height: 60),
+            Container(
+              padding: EdgeInsets.only(top: 3, left: 3),
+              child: MaterialButton(
+                minWidth: double.infinity,
+                height: 60,
+                elevation: 0,
+                color: Color(0xffC31C2B),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginPage()));
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                child: Text(
+                  "Log Out",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      color: Colors.white),
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-    ));
+          ],
+                ),
+              ),
+        ));
   }
 }
